2b984edc6fd2e88c7b45250521dc27079189d962
martin <14334614+martin-jobs@user.noreply.gitee.com>
2025-04-10 13:15:46 +0800

    first

--- zkf-workbench/src/main/java/com/szhuima/zkf/workbench/WorkBenchApplication.java
+++ zkf-workbench/src/main/java/com/szhuima/zkf/workbench/WorkBenchApplication.java
@@ -1,9 +1,21 @@
 package com.szhuima.zkf.workbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
-** @Author: szhuima
-** @Date    2025/4/10 12:57
-** @Description 
-**/
+ * * @Author: szhuima
+ * * @Date    2025/4/10 12:57
+ * * @Description
+ **/
+@EnableScheduling
+@SpringBootApplication
 public class WorkBenchApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkBenchApplication.class, args);
+    }
 
 }
